a61aa4938a814e164732d0711d6aa1e4f1301d90
Kelvin <106881@upf.br>
2016-11-18 20:34:48 -0200

    Add links para páginas no dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -65,22 +65,15 @@
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li class="active"><a href="#">Cadastro de Pessoas<span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Reports</a></li>
-                <li><a href="#">Analytics</a></li>
-                <li><a href="#">Export</a></li>
+                <li class="active"><a href="?">Home<span class="sr-only">(current)</span></a></li>
+                <li><a href="?p=quartos">Quartos</a></li>
+                <li><a href="?p=reservas">Reservas</a></li>
             </ul>
             <ul class="nav nav-sidebar">
-                <li><a href="">Nav item</a></li>
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-                <li><a href="">More navigation</a></li>
-            </ul>
-            <ul class="nav nav-sidebar">
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
+                <li><a href="?p=c_pessoa">Cadastro de pessoas</a></li>
+                <li><a href="?p=c_quarto">Cadastro de quartos</a></li>
+                <li><a href="?p=c_reserva">Cadastro de reservas</a></li>
+                <li><a href="?p=c_tipo">Cadastro de tipo de quarto</a></li>
             </ul>
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">

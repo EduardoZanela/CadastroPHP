a142d32d033176c037366f1d0ab18df816e3378b
Kelvin <106881@upf.br>
2016-11-18 21:13:01 -0200

    Add bootstrap classes

--- view/c_quarto.php
+++ view/c_quarto.php
@@ -1,17 +1,17 @@
 <h2>Cadastro de Quarto</h2>
-<form action="" method="post">
+<form action="" method="post" class="form-group">
 	<!-- @TODO gerar dropdown do "tipoQuarto" -->
-	<input type="number" name="tipoQuarto" placeholder="tipo" required="" /><br>
-	<input type="number" name="numero" placeholder="numero" required="" /><br>
-	<input name="andar" placeholder="andar" required="" /><br>
-	<input name="tv" placeholder="tv" required="" /><br>
-	<input name="banheiro" placeholder="banheiro" required="" /><br>
-	<input name="frigobar" placeholder="frigobar" required="" /><br>
-	<input name="sacada" placeholder="sacada" required="" /><br>
-	<input name="jacuzi" placeholder="jacuzi" required="" /><br>
-	<input name="estado" placeholder="estado" required="" /><br>
-	<input name="cama" placeholder="cama" required="" /><br>
-	<input name="descricao" placeholder="descricao" required="" /><br>
-	<input type="submit" value="Submit" />
-	<input type="reset" />
+	<input type="number" name="tipoQuarto" placeholder="tipo" required="" class="form-control" />
+	<input type="number" name="numero" placeholder="numero" required="" class="form-control" />
+	<input name="andar" placeholder="andar" required="" class="form-control" />
+	<input name="tv" placeholder="tv" required="" class="form-control" />
+	<input name="banheiro" placeholder="banheiro" required="" class="form-control" />
+	<input name="frigobar" placeholder="frigobar" required="" class="form-control" />
+	<input name="sacada" placeholder="sacada" required="" class="form-control" />
+	<input name="jacuzi" placeholder="jacuzi" required="" class="form-control" />
+	<input name="estado" placeholder="estado" required="" class="form-control" />
+	<input name="cama" placeholder="cama" required="" class="form-control" />
+	<input name="descricao" placeholder="descricao" required="" class="form-control" />
+	<input type="submit" value="Submit" class="btn btn-default" class="form-control" />
+	<input type="reset" class="btn btn-default" />
 </form>
